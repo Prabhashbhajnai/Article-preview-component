e40011f2f0a6e120409fcb00688e417767383a3c
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-01-28 22:39:57 +0530

    added share div for mobile devices

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const handleOpen = () => {
     const tooltip = document.getElementById('popover')
+    const userContainer = document.getElementById('user-container')
+    const mobileShare = document.getElementById('mobile-share')
+    const shareBtn = document.getElementById('share-container')
 
-    if (tooltip.style.display === 'none' || tooltip.style.display === '') {
-        tooltip.style.display = 'block'
-        shareBtn.classList.add('active')
+    if (window.innerWidth <= 480) {
+        if (userContainer.style.display === 'flex' || userContainer.style.display === '') {
+            userContainer.style.display = 'none'
+            mobileShare.classList.add('mobileActive')
+            mobileShare.classList.toggle('hide-on-mobile')
+        }
+        else {
+            userContainer.style.display = 'flex'
+            mobileShare.classList.remove('mobileActive')
+            mobileShare.classList.toggle('hide-on-mobile')
+        }
     }
     else {
-        tooltip.style.display = 'none'
-        shareBtn.classList.remove('active')
+        if (tooltip.style.display === 'none' || tooltip.style.display === '') {
+            tooltip.style.display = 'block'
+            shareBtn.classList.add('active')
+        }
+        else {
+            tooltip.style.display = 'none'
+            shareBtn.classList.remove('active')
+        }
+
     }
 }
 
-const shareBtn = document.querySelector('#share-container')
-shareBtn.addEventListener('click', handleOpen)
+const shareBtn = document.querySelectorAll('#share-container')
 
-// if (window.innerWidth <= 480) {
-//     const shareBtn = document.getElementsById('share-container')
-//     shareBtn.addEventListener('click', handleOpen)
-// } else {
-//     const shareBtn = document.getElementsByClassName('share-container')[1]
-//     shareBtn.addEventListener('click', handleOpen)
-// }
+shareBtn.forEach(btn => {
+    btn.addEventListener('click', handleOpen)
+})
